e5273960b6039d96ba962d945269f9d59a9a73d3
asil <9745019+asiill@users.noreply.github.com>
2023-03-10 22:37:25 -0500

    Organize code

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-/*
-Etch-a-Sketch
-*/
+/* Etch-a-Sketch */
 
 // Create variables to store html elements
 const grid = document.querySelector("#grid");
@@ -10,58 +8,15 @@ sliderValue.textContent = gridSlider.value;
 const customColourSelector = document.querySelector("#customColour");
 const resetGrid = document.querySelector("#resetGrid");
 
-// Initialize the grid using the default slider value (16)
-createGrid(gridSlider.value, gridSlider.value);
-
-// Always display the current slider value
-gridSlider.oninput = function() {
-    sliderValue.textContent = this.value;
-}
-
-// Allow the user to toggle the grid dimensions using the slider
-gridSlider.addEventListener("mouseup", reset);
-
-// Clear the grid when the "resetGrid" button is clicked
-resetGrid.addEventListener("click", reset);
-
-// Get the current value of the colour selector
-let customColour = customColourSelector.value; 
-// Update the value of the colour selector and uncheck radio items
-customColourSelector.onchange = function(){
-    customColour = this.value;
-    uncheckRadio();
-}
-// Uncheck radio items even if user selects the same colour
-customColourSelector.addEventListener("click", function() {
-    customColour = this.value;
-    uncheckRadio();
-});
-
-// This function clears the grid
-function reset() {
-    grid.textContent = "";
-    createGrid(gridSlider.value, gridSlider.value);
-    uncheckRadio();
-}
-
-// This function unchecks all radio items
-function uncheckRadio() {
-    let checkedRadioItems = document.querySelectorAll("input[type=radio]:checked");
-    checkedRadioItems.forEach(r => {
-        r.checked = false;
-    });
-}
-
-// This function creates the grid
-function createGrid (rows, columns){
-    grid.style.setProperty("--rows", rows);
-    grid.style.setProperty("--columns", columns);
-    for (let i = 0; i < rows * columns; i++){
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.addEventListener("mouseenter", selectColour);
-        grid.appendChild(cell);
+// This function returns a random HEX colour code
+function generateRandomColour() {
+    const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+    let colour = "#";
+    for (let i = 0; i < 6; i++){
+        let index = Math.floor(Math.random() * 16);
+        colour += values[index];
     }
+    return(colour);
 }
 
 // This function selects a colour based on which radio item is checked
@@ -98,13 +53,57 @@ function selectColour() {
     }
 }
 
-// This function returns a random HEX colour code
-function generateRandomColour() {
-    const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
-    let colour = "#";
-    for (let i = 0; i < 6; i++){
-        let index = Math.floor(Math.random() * 16);
-        colour += values[index];
+// This function creates the grid
+function createGrid (rows, columns){
+    grid.style.setProperty("--rows", rows);
+    grid.style.setProperty("--columns", columns);
+    for (let i = 0; i < rows * columns; i++){
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("mouseenter", selectColour);
+        grid.appendChild(cell);
     }
-    return(colour);
 }
+
+// This function unchecks all radio items
+function uncheckRadio() {
+    let checkedRadioItems = document.querySelectorAll("input[type=radio]:checked");
+    checkedRadioItems.forEach(r => {
+        r.checked = false;
+    });
+}
+
+// This function clears the grid
+function reset() {
+    grid.textContent = "";
+    createGrid(gridSlider.value, gridSlider.value);
+    uncheckRadio();
+}
+
+// Initialize the grid using the default slider value (16)
+createGrid(gridSlider.value, gridSlider.value);
+
+// Always display the current slider value
+gridSlider.oninput = function() {
+    sliderValue.textContent = this.value;
+}
+
+// Allow the user to toggle the grid dimensions using the slider
+gridSlider.addEventListener("mouseup", reset);
+
+// Clear the grid when the "resetGrid" button is clicked
+resetGrid.addEventListener("click", reset);
+
+// Get the current value of the colour selector
+let customColour = customColourSelector.value;
+
+// Update the value of the colour selector and uncheck radio items
+customColourSelector.onchange = function(){
+    customColour = this.value;
+    uncheckRadio();
+}
+// Uncheck radio items even if user selects the same colour
+customColourSelector.addEventListener("click", function() {
+    customColour = this.value;
+    uncheckRadio();
+});
